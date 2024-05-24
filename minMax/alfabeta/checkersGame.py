0ce826c4f634adf95bc65e5a5b8c7f6644821103
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2024-05-23 21:28:00 -0300

    just a pointer in my local machine for the development

--- minMax/alfabeta/checkersGame.py
+++ minMax/alfabeta/checkersGame.py
@@ -1,13 +1,15 @@
 
-board = [
-    ['-','2','-','2','-','2','-','2'],
-    ['2','-','2','-','2','-','2','-'],
-    ['-','2','-','2','-','2','-','2'],
-    ['0','-','0','-','0','-','0','-'],
-    ['-','0','-','0','-','0','-','0'],
-    ['1','-','1','-','1','-','1','-'],
-    ['-','1','-','1','-','1','-','1'],
-    ['1','-','1','-','1','-','1','-']
+
+board = [ 
+   #  0   1   2   3   4   5   6   7
+    ['-','2','-','2','-','2','-','2'],#0
+    ['2','-','2','-','2','-','2','-'],#1
+    ['-','2','-','2','-','2','-','2'],#2
+    ['0','-','2','-','0','-','0','-'],#3
+    ['-','1','-','0','-','0','-','0'],#4
+    ['0','-','1','-','1','-','1','-'],#5
+    ['-','1','-','1','-','1','-','1'],#6
+    ['1','-','1','-','1','-','1','-'] #7
 ]
 
 def printBoard():
@@ -16,9 +18,28 @@ def printBoard():
             print(board[i][j]+ ' ', end='')
         print()
                 
- def possibleMoves(line:int, collumn:int):
+
+def possibleMovesChecker(line:int, collumn:int):
      if line < 8 and collumn < 8 and line >= 0 and collumn >= 0:
-         
-     
+        possibleMoves = set()
+        if board[line][collumn] == '2':
+            if line+1 < 8 and collumn-1 >= 0 and board[line+1][collumn-1] == '0':
+                possibleMoves.add((line+1,collumn-1))
+            elif line+2 < 8 and collumn-2 >= 0 and board[line+1][collumn-1] != board[line][collumn] and board[line+2][collumn-2] == '0':
+                possibleMoves.add((line+2,collumn-2))
+            if line+1 < 8 and collumn+1 < 8 and board[line+1][collumn+1] == '0':
+                possibleMoves.add((line+1,collumn+1))
+            elif line+2 < 8 and collumn+2 < 8 and board[line+1][collumn+1] != board[line][collumn] and board[line+2][collumn+2] == '0':
+                possibleMoves.add((line+2,collumn+2))
+        else:
+            if line-1 >= 0 and collumn-1 >= 0 and board[line-1][collumn-1] == '0':
+                possibleMoves.add((line-1,collumn-1))
+            if line-1 >= 0 and collumn+1 < 8 and board[line-1][collumn+1] == '0':
+                possibleMoves.add((line-1,collumn+1))
+        return possibleMoves         
+    
+              
+              
                 
 printBoard()
+print(possibleMovesChecker(5,0))
