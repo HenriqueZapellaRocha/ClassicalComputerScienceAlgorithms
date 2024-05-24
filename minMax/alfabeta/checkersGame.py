4a9509396b9947d2cb4165a2a1047faa764bc153
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2024-05-23 21:51:41 -0300

    simple movements added

--- minMax/alfabeta/checkersGame.py
+++ minMax/alfabeta/checkersGame.py
@@ -4,10 +4,10 @@ board = [
    #  0   1   2   3   4   5   6   7
     ['-','2','-','2','-','2','-','2'],#0
     ['2','-','2','-','2','-','2','-'],#1
-    ['-','2','-','2','-','2','-','2'],#2
+    ['-','2','-','0','-','2','-','2'],#2
     ['0','-','2','-','0','-','0','-'],#3
     ['-','1','-','0','-','0','-','0'],#4
-    ['0','-','1','-','1','-','1','-'],#5
+    ['1','-','1','-','1','-','1','-'],#5
     ['-','1','-','1','-','1','-','1'],#6
     ['1','-','1','-','1','-','1','-'] #7
 ]
@@ -20,7 +20,7 @@ def printBoard():
                 
 
 def possibleMovesChecker(line:int, collumn:int):
-     if line < 8 and collumn < 8 and line >= 0 and collumn >= 0:
+    if line < 8 and collumn < 8 and line >= 0 and collumn >= 0:
         possibleMoves = set()
         if board[line][collumn] == '2':
             if line+1 < 8 and collumn-1 >= 0 and board[line+1][collumn-1] == '0':
@@ -34,12 +34,16 @@ def possibleMovesChecker(line:int, collumn:int):
         else:
             if line-1 >= 0 and collumn-1 >= 0 and board[line-1][collumn-1] == '0':
                 possibleMoves.add((line-1,collumn-1))
+            elif line-2 >= 0 and collumn-2 >= 0 and board[line-1][collumn-1] != board[line][collumn] and board[line-2][collumn-2] == '0':
+                possibleMoves.add((line-2,collumn-2))
             if line-1 >= 0 and collumn+1 < 8 and board[line-1][collumn+1] == '0':
                 possibleMoves.add((line-1,collumn+1))
+            elif line-2 >= 0 and collumn+2 < 8 and board[line-1][collumn+1] != board[line][collumn] and board[line-2][collumn+2] == '0':
+                possibleMoves.add((line-2,collumn+2))
         return possibleMoves         
-    
+    return None
               
               
                 
 printBoard()
-print(possibleMovesChecker(5,0))
+print(possibleMovesChecker(4,1))
